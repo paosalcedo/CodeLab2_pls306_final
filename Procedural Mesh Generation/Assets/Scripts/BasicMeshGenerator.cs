5adb483daf09064c39175d5b71c74e8f6c44d355
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-12-13 16:15:46 -0500

    Added unity project

---
+++ Procedural Mesh Generation/Assets/Scripts/BasicMeshGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMeshGenerator : MonoBehaviour {
+	Mesh mesh;
+	Vector3[] myVertices;
+	[SerializeField] float speed = 90f;
+	// Use this for initialization
+	void Start () {
+		mesh = new Mesh();
+		//assign points
+
+		Vector3[] vertices = new Vector3[3];
+		vertices[0] = new Vector3(-1, -1, 0);
+		vertices[1] = new Vector3(0, 0.8f, 0);
+		vertices[2] = new Vector3(1, -1, 0);
+		mesh.vertices = vertices;
+
+		int[] triangles =  new int[3] {0, 1, 2};
+		mesh.triangles = triangles;
+
+		GetComponent<MeshFilter>().mesh = mesh; 
+
+		Vector3[] normals = new Vector3[3];
+		normals[0] = Vector3.back; 
+		normals[1] = Vector3.back; 
+		normals[2] = Vector3.back;
+		mesh.normals = normals;
+		myVertices = vertices;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// GenerateTriangles();
+		
+ 	}
+
+	void GenerateTriangles(){
+		int width = 1;
+		int[] triangles_ = new int[(width - 1) * (width-1) * 6];
+		int triangleIndex = 0;
+		for (int x = 0; x < width - 1; x++){
+			for (int y = 0; y < width - 1; y++)
+			{
+				int vertexIndex = x * width + y;
+				
+				triangles_[triangleIndex + 0] = vertexIndex;
+				triangles_[triangleIndex + 1] = vertexIndex + width;
+				triangles_[triangleIndex + 2] = vertexIndex + width + 1;
+				triangles_[triangleIndex + 3] = vertexIndex; 
+				triangles_[triangleIndex + 4] = vertexIndex + width + 1;
+				triangles_[triangleIndex + 5] = vertexIndex + 1;
+
+				triangleIndex += 6;
+			}
+		}
+
+		mesh.triangles = triangles_;
+	}
+}
---
+++ Procedural Mesh Generation/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+	public SquareGrid squareGrid;
+
+	public void GenerateMesh(int[,] map, float squareSize){
+		squareGrid = new SquareGrid(map, squareSize);
+	}
+	public class SquareGrid{
+		public Square[,] squares;
+		public SquareGrid(int[,] map, float squareSize){
+			int nodeCountX = map.GetLength(0);
+			int nodeCountY = map.GetLength(1);
+			float mapWidth = nodeCountX * squareSize;
+			float mapHeight = nodeCountY * squareSize; 
+		}
+	}
+	public class Square {
+		public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+		public Node centerTop, centerRight, centerBottom, centerLeft;
+
+		public Square (ControlNode _topLeft,ControlNode _topRight,ControlNode _bottomRight,ControlNode _bottomLeft){
+			topLeft = _topLeft;
+			topRight = _topRight;
+			bottomRight = _bottomRight;
+			bottomLeft = _bottomLeft;
+
+			centerTop = topLeft.right;
+		}
+	}
+	public class Node {
+		public Vector3 position;
+		public int vertexIndex = -1;
+		public Node(Vector3 _pos){
+			position = _pos;
+		}
+	}
+
+	public class ControlNode : Node {
+		public bool active;
+		public Node above, right;
+		
+		public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos){
+			active = _active;
+			above = new Node(position + Vector3.forward * squareSize/2f);
+			right = new Node (position + Vector3.right * squareSize/2);
+		}
+	}
+	
+}
